5984db510c9567301494077165b71cc2cdc8e778
chinta chandan <chintachandan@gmail.com>
2023-03-10 09:46:24 +0530

    Add files via upload

---
+++ customizations Data.sql
@@ -0,0 +1,6 @@
+insert into customizations (customization_id, dairy_product, sprinkles) values (1, 'Oat Milk', false);
+insert into customizations (customization_id, dairy_product, sprinkles) values (2, 'Coconut Milk', false);
+insert into customizations (customization_id, dairy_product, sprinkles) values (3, 'Reduced Fat Milk', true);
+insert into customizations (customization_id, dairy_product, sprinkles) values (4, 'Whole Milk', true);
+insert into customizations (customization_id, dairy_product, sprinkles) values (5, 'Coconut Milk', false);
+insert into customizations (customization_id, dairy_product, sprinkles) values (6, 'Lactosefree Milk', false);
---
+++ merchandise Data.sql
@@ -0,0 +1,5 @@
+insert into merchandise (merchandise_id, item) values (1, 'Cold Drinkware');
+insert into merchandise (merchandise_id, item) values (2, 'Cold Drinkware');
+insert into merchandise (merchandise_id, item) values (3, 'Machines');
+insert into merchandise (merchandise_id, item) values (4, 'Drinkware');
+insert into merchandise (merchandise_id, item) values (5, 'Accessories');
